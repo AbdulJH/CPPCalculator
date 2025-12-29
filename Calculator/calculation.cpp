c9e6b6eed2111457531b6093d99a8b9e9b9dbd27
Abdul Hussein <abdul.chs21@gmail.com>
2025-12-29 12:51:48 -0600

    Created a header file to use instead of forward declartions. Made logic in calculation.cpp, gather user input from main.cpp

---
+++ Calculator/calculation.cpp
@@ -0,0 +1,15 @@
+double add(double numOne, double numTwo) {
+	return numOne + numTwo;
+}
+
+double subtract(double numOne, double numTwo) {
+	return numOne - numTwo;
+}
+
+double multiply(double numOne, double numTwo) {
+	return numOne * numTwo;
+}
+
+double divide(double numOne, double numTwo) {
+	return numOne / numTwo;
+}
---
+++ Calculator/calculation.h
@@ -0,0 +1,13 @@
+#ifndef CALCULATION_H
+#define CALCULATION_H
+
+double add(double numOne, double numTwo);
+double subtract(double numOne, double numTwo);
+double multiply(double numOne, double numTwo);
+double divide(double numOne, double numTwo);
+
+#endif
+
+
+
+
