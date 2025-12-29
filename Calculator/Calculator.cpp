966fcd477822f637a59e6a4dbe594724b084080c
Abdul Hussein <abdul.chs21@gmail.com>
2025-12-29 12:33:39 -0600

    initial commit

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+// Want to design a calculator 
+// Will have a class that handles logic, add, subtract, divide, multiply
+// User will pick two numbers and we will apply the logic in main
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}
+
