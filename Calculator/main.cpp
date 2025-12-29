fe1a4f95485bb5eeb02113776b02af302cc29d75
Abdul Hussein <abdul.chs21@gmail.com>
2025-12-29 12:59:20 -0600

    created a printResult method, and made the function so the calculator goes once

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "calculation.h"
 
 
 // Want to design a calculator 
@@ -6,9 +7,45 @@
 // User will pick two numbers and we will apply the logic in main
 
 
+void printResult(double numOne, char op, double numTwo, double result)
+{
+    std::cout << numOne << ' ' << op << ' ' << numTwo << " = " << result;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Enter your first number: ";
+    double userFirstNum{};
+    std::cin >> userFirstNum;
+
+    std::cout << "Choose your operand (+ , - , * , /): ";
+    char userOp{};
+    std::cin >> userOp;
+
+    std::cout << "Enter your second number: ";
+    double userSecondNum{};
+    std::cin >> userSecondNum;
+
+    switch (userOp)
+    {
+    case '+':
+        printResult(userFirstNum, userOp, userSecondNum, add(userFirstNum, userSecondNum));
+        break;
+    case '-':
+        printResult(userFirstNum, userOp, userSecondNum, subtract(userFirstNum, userSecondNum));
+        break;
+    case '*':
+        printResult(userFirstNum, userOp, userSecondNum, multiply(userFirstNum, userSecondNum));
+        break;
+    case '/':
+        printResult(userFirstNum, userOp, userSecondNum, divide(userFirstNum, userSecondNum));
+        break;
+    default:
+        std::cout << "ERROR!!";
+        break;
+    }
+
+
     return 0;
 }
 
